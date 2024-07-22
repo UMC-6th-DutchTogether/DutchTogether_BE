c7953e3d9d957268e1af647444b4703c344a28fc
jeondain <dain.jeon03@gmail.com>
2024-07-23 00:13:43 +0900

    Initial commit

---
+++ src/main/java/com/umc/DutchTogether/DutchTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.umc.DutchTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchTogetherApplication.class, args);
+	}
+
+}
