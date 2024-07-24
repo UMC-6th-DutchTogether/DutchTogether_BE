86e29cc3de92a72c7d41c0f06a9bf9f147654d7a
jeondain <dain.jeon03@gmail.com>
2024-07-24 16:31:17 +0900

    [STYLE] 폴더명 변경

--- src/main/java/com/umc/DutchTogether/root/RootController.java
+++ src/main/java/com/umc/DutchTogether/root/RootController.java
@@ -1,4 +1,4 @@
-package com.umc.DutchTogether.Root;
+package com.umc.DutchTogether.root;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
