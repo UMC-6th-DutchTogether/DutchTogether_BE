dc8db620d88c7dbe152929a0b643dc12333a1a4d
heehun <slrkqk2@naver.com>
2024-07-29 17:35:51 +0900

    [FIX] WebConfig 도메인 추가

---
+++ src/main/java/com/umc/DutchTogether/root/RootResponseDTO.java
@@ -0,0 +1,16 @@
+//package com.umc.DutchTogether.root;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//public class RootResponseDTO {
+//    @Builder
+//    @Getter
+//    @NoArgsConstructor
+//    @AllArgsConstructor
+//    public static class TempTestDTO{
+//        String testString;
+//    }
+//}
