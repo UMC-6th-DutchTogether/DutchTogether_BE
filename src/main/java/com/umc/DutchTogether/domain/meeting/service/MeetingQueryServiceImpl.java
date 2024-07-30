8a624bd5bbe5af5c0299ab9d29fd4579b743de45
jeondain <dain.jeon03@gmail.com>
2024-07-30 22:54:22 +0900

    [REFACTOR] Error message 변경

--- src/main/java/com/umc/DutchTogether/domain/meeting/service/MeetingQueryServiceImpl.java
+++ src/main/java/com/umc/DutchTogether/domain/meeting/service/MeetingQueryServiceImpl.java
@@ -4,6 +4,7 @@ import com.umc.DutchTogether.domain.meeting.entity.Meeting;
 import com.umc.DutchTogether.domain.meeting.repository.MeetingRepository;
 import com.umc.DutchTogether.global.apiPayload.code.status.ErrorStatus;
 import com.umc.DutchTogether.global.apiPayload.exception.handler.MeetingHandler;
+import com.umc.DutchTogether.global.apiPayload.exception.handler.SettlementHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +19,7 @@ public class MeetingQueryServiceImpl implements MeetingQueryService{
     @Override
     public String getMeetingLink(Long meetingNum) {
         Meeting meeting = meetingRepository.findById(meetingNum)
-                .orElseThrow(() -> new MeetingHandler(ErrorStatus._BAD_REQUEST));
+                .orElseThrow(() -> new MeetingHandler(ErrorStatus.MEETING_NOT_FOUND));
         return meeting.getLink();
     }
 }
