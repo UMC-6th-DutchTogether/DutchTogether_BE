854301078e165d9d468c193cb81769e90237c5b3
jeondain <dain.jeon03@gmail.com>
2024-08-07 01:54:47 +0900

    [FEAT] S3 세팅

---
+++ src/main/java/com/umc/DutchTogether/global/aws/s3/Uuid.java
@@ -0,0 +1,19 @@
+package com.umc.DutchTogether.global.aws.s3;
+
+import com.umc.DutchTogether.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Uuid extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+}
---
+++ src/main/java/com/umc/DutchTogether/global/aws/s3/UuidRepository.java
@@ -0,0 +1,6 @@
+package com.umc.DutchTogether.global.aws.s3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UuidRepository extends JpaRepository<Uuid, Long> {
+}
