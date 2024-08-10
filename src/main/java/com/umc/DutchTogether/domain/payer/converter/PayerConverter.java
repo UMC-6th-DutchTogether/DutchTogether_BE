e6bd2f9974f3f90256320dd69737ee392b913228
heehun <slrkqk2@naver.com>
2024-08-10 11:08:17 +0900

    [REFACTOR] PayerQueryService - 예외처리 및 컨버터 분리

--- src/main/java/com/umc/DutchTogether/domain/payer/converter/PayerConverter.java
+++ src/main/java/com/umc/DutchTogether/domain/payer/converter/PayerConverter.java
@@ -3,6 +3,9 @@ package com.umc.DutchTogether.domain.payer.converter;
 import com.umc.DutchTogether.domain.payer.dto.PayerRequest;
 import com.umc.DutchTogether.domain.payer.dto.PayerResponse;
 import com.umc.DutchTogether.domain.payer.entity.Payer;
+import com.umc.DutchTogether.domain.settlement.entity.Settlement;
+
+import java.util.List;
 
 public class PayerConverter {
 
@@ -17,6 +20,36 @@ public class PayerConverter {
     public static PayerResponse.PayerDTO toPayerDTO(Payer payer) {
         return PayerResponse.PayerDTO.builder()
                 .payerId(payer.getId())
+                .build();
+    }
+
+    public static PayerResponse.PayerNameDTO toPayerNameDTO(Settlement settlement) {
+        return PayerResponse.PayerNameDTO.builder()
+                .name(settlement.getPayer().getName())
+                .settlementId(settlement.getId())
+                .build();
+    }
+
+    public static PayerResponse.PayerNameListDTO toPayerNameListDTO(List<PayerResponse.PayerNameDTO> payerNames){
+        return PayerResponse.PayerNameListDTO.builder()
+                .names(payerNames)
+                .build();
+    }
+
+    public static PayerResponse.PayerInfoDTO toPayerInfoDTO(Settlement settlement) {
+        Payer payer = settlement.getPayer();
+
+        return PayerResponse.PayerInfoDTO.builder()
+                .totalAmount(settlement.getTotalAmount())
+                .bank(payer.getBank())
+                .accountNum(payer.getAccountNum())
+                .name(payer.getName())
+                .build();
+    }
+
+    public static PayerResponse.PayerInfoListDTO payerInfoListDTO(List<PayerResponse.PayerInfoDTO> payerInfos){
+        return PayerResponse.PayerInfoListDTO.builder()
+                .PayerInfos(payerInfos)
                 .build();
     }
 }
