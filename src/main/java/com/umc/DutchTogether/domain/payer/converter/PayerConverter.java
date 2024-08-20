c97fef97800dcf677e314551436de11bcb836fe5
heehunjung <slrkqk2@naver.com>
2024-08-21 00:36:34 +0900

    [FIX] requestBody - settlementId 추가

--- src/main/java/com/umc/DutchTogether/domain/payer/converter/PayerConverter.java
+++ src/main/java/com/umc/DutchTogether/domain/payer/converter/PayerConverter.java
@@ -42,6 +42,7 @@ public class PayerConverter {
                 .bank(payer.getBank())
                 .accountNum(payer.getAccountNum())
                 .name(payer.getName())
+                .settlementId(settlement.getId())
                 .build();
     }
 
